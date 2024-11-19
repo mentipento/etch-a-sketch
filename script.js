2b35d7d000f61ab18cf37af4e9bf61bdc25852ff
mentipento <26530260+mentipento@users.noreply.github.com>
2024-11-19 16:23:14 +0100

    add button and set up event listener

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function createDivs(x) {
-
-for (let i = 0; i < x**2; i++) {
+function createDivs(x = 16) {
     const container = document.querySelector("#container");
-    let square = document.createElement("div");
-    square.style.cssText = `flex: 1 0 calc(100%/${x})`;
-    container.appendChild(square);
+    container.innerHTML = "";
+
+    for (let i = 0; i < x ** 2; i++) {
+        let square = document.createElement("div");
+        square.style.cssText = `flex: 1 0 calc(100% / ${x});`;
+        container.appendChild(square);
+    }
 }
-}
-let x = 20;
-createDivs(x);
+
+createDivs();
+
+const button = document.querySelector("#button");
+button.addEventListener("click", () => {
+    let input = Number(prompt("Please input an integer number between 2 and 100", "16"));
+
+    if (input > 100 || input < 2 || !Number.isInteger(input)) {
+        alert("Please enter an integer between 2 and 100");
+        createDivs();
+    } else {
+        createDivs(input);
+    }
+});
