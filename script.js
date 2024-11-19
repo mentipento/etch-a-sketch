150815eeba293421b8d1f043080f8c687e668b63
mentipento <26530260+mentipento@users.noreply.github.com>
2024-11-19 15:32:02 +0100

    allow for flexible amount of squares <= 100

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function createDivs(x) {
 for (let i = 0; i < x**2; i++) {
     const container = document.querySelector("#container");
     let square = document.createElement("div");
-    square.classList.add("square");
+    square.style.cssText = `flex: 1 0 calc(100%/${x})`;
     container.appendChild(square);
 }
 }
-let x = 16;
+let x = 20;
 createDivs(x);
